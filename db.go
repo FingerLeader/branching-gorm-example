26e66acfbda81b91cdd70ed71c9c4b43f6bc5c28
FingerLeader <wanxfinger@gmail.com>
2024-10-14 02:41:33 +0800

    1
    Signed-off-by: FingerLeader <wanxfinger@gmail.com>

--- db.go
+++ db.go
@@ -120,7 +120,7 @@ func RunMigrations() {
 			// alter table
 			ID: "202307111200",
 			Migrate: func(db *gorm.DB) error {
-				if err := db.Exec("ALTER TABLE `users` ADD `new2_field` varchar(255) NOT NULL").Error; err != nil {
+				if err := db.Exec("ALTER TABLE `users` ADD `new3_field` varchar(255) NOT NULL").Error; err != nil {
 					return err
 				}
 				return nil
